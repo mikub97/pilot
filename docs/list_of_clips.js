599274c2ab8533b9f24ff471a18999912f967b80
Michał Weiss <mikub97@gmail.com>
2026-02-05 17:56:57 +0100

    revert

--- docs/list_of_clips.js
+++ docs/list_of_clips.js
@@ -1,5 +1,14 @@
 // Generated on 2026-02-05 17:18:14
 const videoClips = [
-    "assets/videos/cut_clips/20260205_154236_clip_04_203.2-218.4s.mp4"
+    "assets/videos/cut_clips/20260205_154236_clip_04_203.2-218.4s.mp4",
+    "assets/videos/cut_clips/20260205_154236_clip_12_539.9-555.1s.mp4",
+    "assets/videos/cut_clips/20260205_154236_clip_19_913.0-940.3s.mp4",
+    "assets/videos/cut_clips/20260205_154236_clip_20_964.6-976.7s.mp4",
+    "assets/videos/cut_clips/20260205_154236_clip_27_1240.2-1250.2s.mp4",
+    "assets/videos/cut_clips/20260205_154236_clip_31_1306.9-1316.9s.mp4",
+    "assets/videos/cut_clips/20260205_154236_clip_36_1401.4-1413.5s.mp4",
+    "assets/videos/cut_clips/20260205_154236_clip_37_1440.4-1450.4s.mp4",
+    "assets/videos/cut_clips/20260205_154236_clip_38_1459.0-1471.2s.mp4",
+    "assets/videos/cut_clips/20260205_154236_clip_39_1483.3-1501.5s.mp4"
 ];
 const numClips = videoClips.length;
